ef9053413e5143f9abb2fae670b17c828338f4d0
tropicbliss <tropicbliss@protonmail.com>
2022-07-18 21:18:09 +0800

    E

---
+++ README.md
@@ -0,0 +1,21 @@
+# Cracker
+
+Reverses Minecraft seed strings from a given seed with regex patterns.
+
+## Compiling from source
+
+If you are on another platform, compile the binary yourself to try it out:
+
+```sh
+git clone https://github.com/tropicbliss/cracker
+cd cracker
+cargo build --release
+```
+
+Compiling from source requires the latest stable version of Rust. Older Rust versions may be able to compile `cracker`, but they are not guaranteed to keep working.
+
+The binary will be located in `target/release`.
+
+## Usage
+
+Use the `--help` command line flag.
---
+++ src/main.rs
@@ -0,0 +1,65 @@
+use anyhow::Result;
+use clap::Parser;
+use rand::{prelude::SmallRng, Rng, SeedableRng};
+use std::num::Wrapping;
+
+fn main() -> Result<()> {
+    let args = Args::parse();
+    let mut parser = regex_syntax::ParserBuilder::new().unicode(false).build();
+    let hir = parser.parse(&args.regex)?;
+    let gen = rand_regex::Regex::with_hir(hir, 100)?;
+    let available_parallelism = std::thread::available_parallelism()?;
+    let mut handles = Vec::new();
+    if args.sample {
+        let mut rng = SmallRng::from_entropy();
+        let seed_string: String = (&mut rng).sample(&gen);
+        println!("Sample: {seed_string}");
+        println!();
+    }
+    for _ in 0..usize::from(available_parallelism) {
+        let gen = gen.clone();
+        let handle = std::thread::spawn(move || {
+            let mut rng = SmallRng::from_entropy();
+            loop {
+                let seed_string: String = (&mut rng).sample(&gen);
+                let seed = java_hashcode(&seed_string);
+                if seed == args.seed {
+                    println!("{seed_string}");
+                }
+            }
+        });
+        handles.push(handle);
+    }
+    for handle in handles {
+        handle.join().unwrap();
+    }
+    Ok(())
+}
+
+#[derive(Parser, Debug)]
+#[clap(version, about)]
+struct Args {
+    /// Generates random strings based on this regex
+    #[clap(short, long)]
+    regex: String,
+
+    /// Seed to compare against
+    #[clap(short, long)]
+    seed: usize,
+
+    /// Gives the maximum extra repeat counts the x*, x+ and x{n,} operators will become
+    #[clap(short, long, default_value = "100")]
+    max_repeat: usize,
+
+    /// Prints a sample generated seed string before attempting to find suitable seed strings
+    #[clap(long)]
+    sample: bool,
+}
+
+fn java_hashcode(string: &str) -> usize {
+    let mut hash = Wrapping(0);
+    for ch in string.chars() {
+        hash = Wrapping(31) * hash + Wrapping(ch as usize);
+    }
+    hash.0
+}
